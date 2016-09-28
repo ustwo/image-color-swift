fa3c8ae651bed85e570b332d1407bf91b9344b01
Daniela Dias <danieladias70@gmail.com>
2016-09-28 17:41:51 +0100

    Conversion to Swift 3.0.

--- UIImageColor/ViewController.swift
+++ UIImageColor/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private(set) var button: UIButton!
+    fileprivate(set) var button: UIButton!
 
 
     override func viewDidLoad() {
@@ -23,29 +23,29 @@ class ViewController: UIViewController {
 
         // Create image from color 
 
-        let image = UIImage.imageFromColor(UIColor.blueColor())
-        let highlightedImage = UIImage.imageFromColor(UIColor.purpleColor())
+        let image = UIImage.imageFromColor(UIColor.blue)
+        let highlightedImage = UIImage.imageFromColor(UIColor.purple)
 
-        button.setBackgroundImage(image, forState: .Normal)
-        button.setBackgroundImage(highlightedImage, forState: .Highlighted)
+        button.setBackgroundImage(image, for: UIControlState())
+        button.setBackgroundImage(highlightedImage, for: .highlighted)
 
         // Create image from color with custom sizes
 
-        let navigationBarBackgroundImage = UIImage.imageFromColor(UIColor.grayColor(), width: 1.0, height: 2.0)
-        let navigationBarShadowImage = UIImage.imageFromColor(UIColor.blackColor().colorWithAlphaComponent(0.8), width: 1.0, height: 2.0)
+        let navigationBarBackgroundImage = UIImage.imageFromColor(UIColor.gray, width: 1.0, height: 2.0)
+        let navigationBarShadowImage = UIImage.imageFromColor(UIColor.black.withAlphaComponent(0.8), width: 1.0, height: 2.0)
 
         self.navigationController?.navigationBar.shadowImage = navigationBarShadowImage
-        self.navigationController?.navigationBar.setBackgroundImage(navigationBarBackgroundImage, forBarMetrics: .Default)
+        self.navigationController?.navigationBar.setBackgroundImage(navigationBarBackgroundImage, for: .default)
     }
 
 
     // MARK: - Setup
 
-    private func setupButton() {
+    fileprivate func setupButton() {
 
         button = UIButton()
-        button.setTitle("Button", forState: .Normal)
-        button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        button.setTitle("Button", for: UIControlState())
+        button.setTitleColor(UIColor.white, for: UIControlState())
         button.clipsToBounds = true
         button.layer.cornerRadius = 5.0
         button.adjustsImageWhenHighlighted = false
@@ -54,10 +54,10 @@ class ViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
 
         let constraints = [
-            NSLayoutConstraint(item: button, attribute: .CenterX, relatedBy: .Equal, toItem: button.superview, attribute: .CenterX, multiplier: 1.0, constant: 0.0),
-            NSLayoutConstraint(item: button, attribute: .CenterY, relatedBy: .Equal, toItem: button.superview, attribute: .CenterY, multiplier: 1.0, constant: 0.0),
-            NSLayoutConstraint(item: button, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .Height, multiplier: 1.0, constant: 44.0),
-            NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .Width, multiplier: 1.0, constant: 100.0)
+            NSLayoutConstraint(item: button, attribute: .centerX, relatedBy: .equal, toItem: button.superview, attribute: .centerX, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: button, attribute: .centerY, relatedBy: .equal, toItem: button.superview, attribute: .centerY, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: button, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 44.0),
+            NSLayoutConstraint(item: button, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1.0, constant: 100.0)
         ]
         view.addConstraints(constraints)
     }
